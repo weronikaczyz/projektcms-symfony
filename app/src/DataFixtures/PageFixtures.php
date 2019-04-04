30028b4bb1140b7fa31ff3c1a38055c46b9af503
Aleksander Gustkiewicz <agustkiewicz@spyro-soft.com>
2019-04-04 20:35:39 +0200

    user

--- app/src/DataFixtures/PageFixtures.php
+++ app/src/DataFixtures/PageFixtures.php
@@ -5,34 +5,20 @@
 namespace App\DataFixtures;
 
 use App\Entity\Page;
-use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
-use Faker\Factory;
 
 /**
  * Class PageFixtures
  */
-class PageFixtures extends Fixture
+class PageFixtures extends AbstractBaseFixtures
 {
     /**
-    * Faker.
+    * Load.
     *
-    * @var \Faker\Generator
+    * @param \Doctrine\Common\Persistence\ObjectManager $manager
     */
-    protected $faker;
-
-    /**
-    * Object manager.
-    *
-    * @var \Doctrine\Common\Persistence\ObjectManager
-    */
-    protected $manager;
-
-    public function load(ObjectManager $manager)
+    public function loadData(ObjectManager $manager): void
     {
-        $this->faker = Factory::create();
-        $this->manager = $manager;
-
         for ($i = 0; $i < 10; ++$i) {
             $page = new Page();
             $page->setTitle($this->faker->sentence);
